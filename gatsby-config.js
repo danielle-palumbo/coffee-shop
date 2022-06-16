49b65f2d1d25d30e93c10aca7beee652a070271b
danielle-palumbo <52574345+datwd@users.noreply.github.com>
2022-06-16 10:38:42 -0400

    Add index page data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: 'The Coffee Blog'
+    title: "The Coffee Blog",
   },
-    plugins: [
-      'gatsby-plugin-netlify-cms',
-      {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-          name: 'blog',
-          path: 'src/blog'
-        }
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: "src/blog",
       },
-      'gatsby-transformer-remark'
-    ]
-  };
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pageData",
+        path: "src/pageData",
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
+}
